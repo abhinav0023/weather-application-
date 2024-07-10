78dac84e82e6d992a17ed06f9df0e5790f1b2692
Abhinav <abhinavhooda223@gmail.com>
2024-07-10 16:18:08 +0530

    completed the upper part of details component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Buttons from './components/Buttons'
 import Input from './components/Input'
 import TimeAndLocation from './components/TimeAndLocation'
+import Details from './components/Details'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <Buttons/>
     <Input/>
     <TimeAndLocation/>
+    <Details/>
     </div>
     </>
   )
